db5b3c3362939cf049f0d4ca287ad403aa9a4e93
Geoffrey Hazard <Geoffrey_Hazard@brown.edu>
2022-12-11 16:22:50 -0800

    create Samples folder with unique ids for samples and csv for description, origin

--- extract_samples.py
+++ extract_samples.py
@@ -6,18 +6,29 @@ from scipy.io import wavfile
 from scipy import signal
 import numpy as np
 import struct
+import uuid
+import csv
+import os
+
 
 # Set the threshold value for identifying drum segments
-threshold = 0.25
+threshold = 0.255
 freq_range = [100, 1000]
 
 def extract_drum_samples(audio_file):
   #print "Extracting drum samples from " + audio_file
   print("Extracting drum samples from " + audio_file)
 
+  # Create a Samples directory if it doesn't already exist
+  if not os.path.exists("Samples"):
+    os.makedirs("Samples")
+
+  # Open the csv file for writing and create a csv writer object
+  csv_file = open("Samples/samples.csv", "a")
+  csv_writer = csv.writer(csv_file)
+
   # Read the audio file and get the sample rate and data
   sample_rate, data = wavfile.read(audio_file)
-
 
   # Use a bandpass filter to isolate the frequency range of drums
   filtered_data = bandpass_filter(data, sample_rate, freq_range)
@@ -37,15 +48,24 @@ def extract_drum_samples(audio_file):
     # Convert the sample data from floating-point numbers to bytes
     byte_data = struct.pack('f' * len(sample_data), *sample_data)
 
-    sample_file = wave.open("drum_sample.wav", "wb")
+    # Generate a unique number id for the sample
+    sample_id = str(uuid.uuid4())
+
+    # Create a .wav file for the sample with the unique id as the file name
+    sample_file = wave.open("Samples/" + sample_id + ".wav", "wb")
+
     sample_file.setnchannels(1)
     sample_file.setsampwidth(2)
     sample_file.setframerate(sample_rate)
 
     # Write the byte data to the file
     sample_file.writeframes(byte_data)
-    drum_samples.append("drum_sample.wav")
+    drum_samples.append(sample_id + ".wav")
 
+    # Write a row to the csv file with the sample id and the file name
+    csv_writer.writerow([sample_id, audio_file])
+  
+  csv_file.close()
   return drum_samples
 
 def detect_drum_segments(audio_data, sample_rate):
@@ -59,19 +79,23 @@ def detect_drum_segments(audio_data, sample_rate):
 
   # Iterate through the audio data and detect drum segments
   for i, sample in enumerate(audio_data):
+    # Check if any element of the sample is above the threshold
     if np.any(sample > threshold):
       # If this is the start of a drum segment, record the start time
       if start_time is None:
         start_time = i / sample_rate
       
       # If this is the end of a drum segment, record the end time and add the segment to the list
-      elif end_time is None:
-        end_time = i / sample_rate
+      elif end_time is not None:
         drum_segments.append((start_time, end_time))
 
         # Reset the start and end times for the next segment
         start_time = None
         end_time = None
+
+    # If we are in the middle of a drum segment, update the end time
+    elif start_time is not None:
+      end_time = i / sample_rate
 
   return drum_segments
 
--- internet_sampler.py
+++ internet_sampler.py
@@ -1,12 +1,20 @@
 #! /usr/bin/python
 
 import extract_samples
+import os
 
 if __name__ == '__main__' :
+
+    # Delete Samples directory and files inside it if it already exists
+    if os.path.exists("Samples"):
+        for file in os.listdir("Samples"):
+            os.remove("Samples/" + file)
+        os.rmdir("Samples")
+
     drum_samples = extract_samples.extract_drum_samples("Data/Birthday.wav")
     print("Found " + str(len(drum_samples)) + " drum samples")
 
-    drum_samples = extract_samples.extract_drum_samples("Data/Standup.wav")
-    print("Found " + str(len(drum_samples)) + " drum samples")
+    # drum_samples = extract_samples.extract_drum_samples("Data/Standup.wav")
+    # print("Found " + str(len(drum_samples)) + " drum samples")
     
     print("Done!")
